9de77e1778b19a471d5e6f0443a3f06a8a3a4e74
koly <me.koly.li@gmail.com>
2012-11-19 16:07:55 +0800

    add the dialog to show loading image message, but the app does not load the message initially

--- src/li/koly/nasaimage/MainActivity.java
+++ src/li/koly/nasaimage/MainActivity.java
@@ -5,14 +5,20 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	IotdHandler handler = null;
+	Handler androidHandler = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,8 @@ public class MainActivity extends Activity {
 			System.out.println("The network connection is not ready!");
 		}
 		setContentView(R.layout.activity_main);
+		androidHandler = new Handler();
+		
 	}
 
 	@Override
@@ -35,12 +43,12 @@ public class MainActivity extends Activity {
 	}
 
 	private class DownLoadNASARss extends AsyncTask {
-		IotdHandler handler = null;
 
 		@Override
 		protected Object doInBackground(Object... params) {
 			// TODO Auto-generated method stub
-			handler = new IotdHandler();
+			if (handler == null)
+				handler = new IotdHandler();
 			handler.processFeed();
 			return null;
 		}
@@ -51,21 +59,42 @@ public class MainActivity extends Activity {
 				resetDisplay(handler.getTitle(), handler.getDate(),
 						handler.getImage(), handler.getDescription());
 		}
+	}
 
-		private void resetDisplay(String title, String date, Bitmap image,
-				StringBuffer description) {
-			TextView titleView = (TextView) findViewById(R.id.image_title);
-			titleView.setText(handler.getTitle());
+	private void resetDisplay(String title, String date, Bitmap image,
+			StringBuffer description) {
+		TextView titleView = (TextView) findViewById(R.id.image_title);
+		titleView.setText(handler.getTitle());
 
-			TextView dateView = (TextView) findViewById(R.id.image_date);
-			dateView.setText(handler.getDate());
+		TextView dateView = (TextView) findViewById(R.id.image_date);
+		dateView.setText(handler.getDate());
 
-			ImageView imageView = (ImageView) findViewById(R.id.image);
-			imageView.setImageBitmap(handler.getImage());
+		ImageView imageView = (ImageView) findViewById(R.id.image);
+		imageView.setImageBitmap(handler.getImage());
 
-			TextView descriptionView = (TextView) findViewById(R.id.image_description);
-			descriptionView.setText(handler.getDescription());
+		TextView descriptionView = (TextView) findViewById(R.id.image_description);
+		descriptionView.setText(handler.getDescription());
+	}
 
-		}
+	public void refreshFromFeed(View view) {
+		final Dialog dialog = ProgressDialog.show(this, "Loading",
+				"Loading the image of the day");
+		Thread th = new Thread() {
+			@Override
+			public void run() {
+				if (handler == null)
+					handler = new IotdHandler();
+				handler.processFeed();
+				androidHandler.post(new Runnable() {
+					@Override
+					public void run() {
+						resetDisplay(handler.getTitle(), handler.getDate(),
+								handler.getImage(), handler.getDescription());
+						dialog.dismiss();
+					}
+				});
+			}
+		};
+		th.start();
 	}
 }
